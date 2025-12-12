ce1dcb03abe43c80e15d4fe303dcf41217401640
hussainp <hussainpithawala@gmail.com>
2025-12-12 21:27:01 +0530

    fixed broken import of Waypoint.js Also a minor change in rendering name

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,28 @@
 (function() {
   "use strict";
 
+  const fadeInElements = document.querySelectorAll('.fade-in-element');
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+  fadeInElements.forEach(element => {
+    observer.observe(element);
+  })
+
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
